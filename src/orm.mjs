5f3b0b55c007ac0a3b63e77fd7c8774eca948f52
c.kokoska <c.kokoska@ceyoniq.com>
2025-02-04 23:41:21 +0100

    orm improved

--- src/orm.mjs
+++ src/orm.mjs
@@ -1,5 +1,15 @@
 import { DatabaseSync } from 'node:sqlite';
 
+export const removeProps = (obj, props) => {
+  const instCopy = {};
+  Object.entries(obj).forEach((item) => {
+    if (!props.includes(item[0])) {
+      instCopy[item[0]] = item[1];
+    }
+  });
+  return instCopy;
+}
+
 export class DbObject {
   static dbInstance = null;
 
@@ -14,10 +24,13 @@ export class DbObject {
   };
 
   constructor(results) {
-    this.constructor.fieldNames.forEach(fieldname => {
-      this[fieldname] = results[fieldname];
-    })
+    if (results) {
+      this.constructor.fieldNames.forEach(fieldname => {
+        this[fieldname] = results[fieldname];
+      })
+    }
   }
+
   static getAll = function () {
     const query = this.database.prepare(`SELECT ${this.fieldNames.join(', ')} FROM ${this.tableName} ORDER BY id`);
     return query.all().map(res => new this(res));
@@ -25,8 +38,40 @@ export class DbObject {
 
   static getByFilter = function (filter) {
     // filter: { firstName: 'Jack', lastName: 'Shephard' }
-    const filterStrings = Object.entries(filter).map(item => `${item[0]}='${item[1]}'`);
-    const query = this.database.prepare(`SELECT ${this.fieldNames.join(', ')} FROM ${this.tableName} WHERE ${filterStrings.join(' AND ')} ORDER BY id`);
-    return query.all().map(res => new this(res));
+    if (!filter) {
+      return [];
+    }
+    const conditions = Object.keys(filter).map(field => `${field} = ?`).join(' AND ');
+    const values = Object.values(filter);
+    const query = this.database.prepare(`SELECT ${this.fieldNames.join(', ')} FROM ${this.tableName} WHERE ${conditions} ORDER BY id`);
+    return query.all(...values).map(res => new this(res));
+  }
+
+  static create = function (item) {
+    let fieldNames = this.fieldNames;
+    if (!item['id']) {
+      fieldNames = this.fieldNames.slice(0, -1);
+    }
+    const query = `INSERT INTO ${this.tableName} (${Object.keys(item).join(', ')}) VALUES (${fieldNames.map(() => '?').join(', ')})`;
+    const prepared = this.database.prepare(query);
+    const lastId = prepared.run(...Object.values(item)).lastInsertRowid;
+    return this.getByFilter({ id: lastId })[0];
+  }
+
+  save = function () {
+    const instCopy = removeProps(this, ['save']);
+
+    let id = this.id;
+
+    if (id) {
+      const query = `UPDATE ${this.constructor.tableName} SET ${Object.keys(instCopy).map((fieldName) => `${fieldName} = ?`).join(', ')}${id ? ' WHERE id = ?' : ''}`;
+      const prepared = this.constructor.database.prepare(query);
+      prepared.run(...Object.values(instCopy), this.id);
+      const filterQuery = this.constructor.database.prepare(`SELECT ${this.constructor.fieldNames.join(', ')} FROM ${this.constructor.tableName} WHERE id = ? ORDER BY id`);
+      return filterQuery.all(id).map(res => new this.constructor(res))[0];
+    } else {
+      return this.constructor.create(instCopy);
+    }
+
   }
 };
--- test/orm.test.mjs
+++ test/orm.test.mjs
@@ -1,7 +1,7 @@
 import fs from 'fs';
 import { test, mock } from 'node:test';
 import assert from 'node:assert';
-import { DbObject } from '../src/orm.mjs';
+import { DbObject, removeProps } from '../src/orm.mjs';
 
 DbObject.setDb(':memory:');
 
@@ -18,8 +18,8 @@ const initDb = () => {
 
   insert.run('Jack', 'Shephard', 'm');
   insert.run('Hugo', 'Reyes', 'm');
-  insert.run('Jin-Soo', 'Kwon', 'm');
   insert.run('Kate', 'Austen', 'f');
+  insert.run('Jin-Soo', 'Kwon', 'm');
   insert.run('Claire', 'Littleton', 'f');
   insert.run('Sun-Hwa', 'Kwon', 'f');
 }
@@ -31,8 +31,55 @@ class User extends DbObject {
   static fieldNames = ['id', 'firstName', 'lastName', 'gender'];
 }
 
-test('simple dbobject', (t) => {
-
+test('get all', (_t) => {
   const users = User.getAll();
-  console.log(users)
+  assert.deepEqual(removeProps(users[0], ['save']), { firstName: 'Jack', lastName: 'Shephard', gender: 'm', id: 1 });
+  assert.deepEqual(removeProps(users[5], ['save']), { firstName: 'Sun-Hwa', lastName: 'Kwon', gender: 'f', id: 6 });
+});
+
+test('get by filter, id', (_t) => {
+  const users = User.getByFilter({id: 3});
+  assert.equal(users.length, 1);
+  assert.deepEqual(removeProps(users[0], ['save']), { firstName: 'Kate', lastName: 'Austen', gender: 'f', id: 3 });
+});
+
+test('get by filter, multiple results', (_t) => {
+  const users = User.getByFilter({ gender: 'm' });
+  assert.equal(users.length, 3);
+  assert.deepEqual(removeProps(users[0], ['save']), { firstName: 'Jack', lastName: 'Shephard', gender: 'm', id: 1 });
+  assert.deepEqual(removeProps(users[1], ['save']), { firstName: 'Hugo', lastName: 'Reyes', gender: 'm', id: 2 });
+  assert.deepEqual(removeProps(users[2], ['save']), { firstName: 'Jin-Soo', lastName: 'Kwon', gender: 'm', id: 4 });
+});
+
+test('get by filter, firstName and gender', (_t) => {
+  const users = User.getByFilter({ firstName: 'Claire', gender: 'f' });
+  assert.equal(users.length, 1);
+  assert.deepEqual(removeProps(users[0], ['save']), { firstName: 'Claire', lastName: 'Littleton', gender: 'f', id: 5 });
+});
+
+test('create', (_t) => {
+  assert.equal(User.getAll().length, 6);
+  const user = User.create({ firstName: 'Michel', lastName: 'Dawson', gender: 'm', id: 7 });
+  assert.equal(User.getAll().length, 7);
+  // Typo in firstName is intended. Will bei fixed in next test.
+  assert.deepEqual(removeProps(user, ['save']), { firstName: 'Michel', lastName: 'Dawson', gender: 'm', id: 7 });
+});
+
+test('save existing user', (_t) => {
+  const user = User.getByFilter({id: 7})[0];
+  assert.deepEqual(removeProps(user, ['save']), { firstName: 'Michel', lastName: 'Dawson', gender: 'm', id: 7 });
+  user.firstName = 'Michael';
+  const updatedUser = user.save();
+  assert.deepEqual(removeProps(updatedUser, ['save']), { firstName: 'Michael', lastName: 'Dawson', gender: 'm', id: 7 });
+  // Self referencing..
+  assert.deepEqual(removeProps(user, ['save']), { firstName: 'Michael', lastName: 'Dawson', gender: 'm', id: 7 });
+});
+
+test('save new user', (_t) => {
+  let user = new User();
+  user.firstName = 'Desmond';
+  user.lastName = 'Hume';
+  user.gender = 'm';
+  user = user.save();
+  assert.deepEqual(removeProps(user, ['save']), { firstName: 'Desmond', lastName: 'Hume', gender: 'm', id: 8 });
 });
